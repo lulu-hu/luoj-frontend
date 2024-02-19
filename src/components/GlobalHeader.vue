c6fe5069939cb62bc1c535d4a4d359567d5202da
jack <jack@guigu.com>
2024-02-19 16:56:03 +0900

    2.19 写到用户权限

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,25 +1,61 @@
 <template>
-  <div id="globalHeader">
-    <a-menu mode="horizontal" :default-selected-keys="['1']">
-      <a-menu-item
-        key="0"
-        :style="{ padding: 0, marginRight: '38px' }"
-        disabled
+  <a-row id="globalHeader" style="margin-bottom: 16px" align="center">
+    <a-col flex="auto">
+      <a-menu
+        mode="horizontal"
+        :selected-keys="selectedKeys"
+        :default-selected-keys="['1']"
+        @menu-item-click="doMenuClick"
       >
-        <div class="title-bar">
-          <img class="logo" src="../assets/OIP-C.jpg" />
-          <div class="title">虂 OJ</div>
-        </div>
-      </a-menu-item>
-      <a-menu-item v-for="item in routes" :key="item.path">{{
-        item.name
-      }}</a-menu-item>
-    </a-menu>
-  </div>
+        <a-menu-item
+          key="0"
+          :style="{ padding: 0, marginRight: '38px' }"
+          disabled
+        >
+          <div class="title-bar">
+            <img class="logo" src="../assets/OIP-C.jpg" />
+            <div class="title">虂 OJ</div>
+          </div>
+        </a-menu-item>
+        <a-menu-item v-for="item in routes" :key="item.path"
+          >{{ item.name }}
+        </a-menu-item>
+      </a-menu>
+    </a-col>
+    <a-col flex="100px">
+      <div>{{ store.state.user?.loginUser?.userName ?? "未登录" }}</div>
+    </a-col>
+  </a-row>
 </template>
 
 <script setup lang="ts">
 import { routes } from "../router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import { useStore } from "vuex";
+
+const router = useRouter();
+
+// 默认主页
+const selectedKeys = ref(["/"]);
+
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+const store = useStore();
+setTimeout(() => {
+  store.dispatch("user/getLoginUser", {
+    userName: "lulu",
+    role: "admin",
+  });
+}, 3000);
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
 </script>
 
 <style scoped>
