7c1cb95baed29a5b2867d9b78c3ebc9e6345ffaf
jack <jack@guigu.com>
2024-06-30 17:04:20 +0800

    feat: 完善了在线做题页面

--- src/components/CodeEditor.vue
+++ src/components/CodeEditor.vue
@@ -1,24 +1,59 @@
 <template>
-  <div id="code-editor" ref="codeEditorRef" style="min-height: 400px" />
+  <div
+    id="code-editor"
+    ref="codeEditorRef"
+    style="min-height: 500px; height: 70vh"
+  />
   <!--  {{ value }}-->
   <!--  <a-button @click="fillValue">填充值</a-button>-->
 </template>
 
 <script setup lang="ts">
 import * as monaco from "monaco-editor";
-import { onMounted, ref, toRaw } from "vue";
+import { onMounted, ref, toRaw, defineProps, withDefaults, watch } from "vue";
+import { languages } from "monaco-editor";
+
+interface Props {
+  value: string;
+  language?: string;
+  handleChange: (v: string) => void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  language: () => "java",
+  handleChange: (v: string) => {
+    console.log(v);
+  },
+});
 
 const codeEditorRef = ref();
 const codeEditor = ref();
-const value = ref("hello world");
 
-const fillValue = () => {
-  if (!codeEditor.value) {
-    return;
-  }
-  // 改变值
-  toRaw(codeEditor.value).setValue("新的值");
-};
+// const fillValue = () => {
+//   if (!codeEditor.value) {
+//     return;
+//   }
+//   // 改变值
+//   toRaw(codeEditor.value).setValue("新的值");
+// };
+
+// watch([props.language], () => {
+//   codeEditor.value = monaco.editor.create(codeEditorRef.value, {
+//     value: props.value,
+//     language: props.language,
+//     automaticLayout: true,
+//     colorDecorators: true,
+//     minimap: {
+//       enabled: true,
+//     },
+//     readOnly: false,
+//     theme: "vs-dark",
+//     // lineNumbers: "off",
+//     // roundedSelection: false,
+//     // scrollBeyondLastLine: false,
+//   });
+// });
 
 onMounted(() => {
   if (!codeEditorRef.value) {
@@ -26,8 +61,8 @@ onMounted(() => {
   }
   // Hover on each property to see its docs!
   codeEditor.value = monaco.editor.create(codeEditorRef.value, {
-    value: value.value,
-    language: "java",
+    value: props.value,
+    language: props.language,
     automaticLayout: true,
     colorDecorators: true,
     minimap: {
@@ -42,7 +77,7 @@ onMounted(() => {
 
   // 编辑 监听内容变化
   codeEditor.value.onDidChangeModelContent(() => {
-    console.log("目前内容为：", toRaw(codeEditor.value).getValue());
+    props.handleChange(toRaw(codeEditor.value).getValue());
   });
 });
 </script>
---
+++ src/views/question/ViewQuestionView.vue
@@ -0,0 +1,146 @@
+<template>
+  <div id="viewQuestionView">
+    <a-row :gutter="[24, 24]">
+      <a-col :md="12" :xs="24">
+        <a-tabs default-active-key="question">
+          <a-tab-pane key="question" title="题目">
+            <a-card v-if="question" :title="question.title">
+              <a-descriptions
+                title="判题条件"
+                :column="{ xs: 1, md: 3, lg: 4 }"
+              >
+                <a-descriptions-item label="时间限制">
+                  {{ question.judgeConfig?.timeLimit ?? 0 }}
+                </a-descriptions-item>
+                <a-descriptions-item label="内存限制">
+                  {{ question.judgeConfig?.memoryLimit ?? 0 }}
+                </a-descriptions-item>
+                <a-descriptions-item label="堆栈限制">
+                  {{ question.judgeConfig?.stackLimit ?? 0 }}
+                </a-descriptions-item>
+              </a-descriptions>
+              <MdViewer :value="question.content" />
+              <template #extra>
+                <a-space wrap>
+                  <a-tag
+                    v-for="(tag, index) of question.tags"
+                    :key="index"
+                    color="gold"
+                    >{{ tag }}
+                  </a-tag>
+                </a-space>
+              </template>
+            </a-card>
+          </a-tab-pane>
+          <a-tab-pane key="comment" title="评论" disabled> 评论区</a-tab-pane>
+          <a-tab-pane key="answer" title="答案"> 答案</a-tab-pane>
+        </a-tabs>
+      </a-col>
+      <a-col :md="12" :xs="24">
+        <a-form :model="form" layout="inline">
+          <a-form-item
+            field="language"
+            label="编程语言"
+            style="min-width: 280px"
+          >
+            <a-select
+              v-model="form.language"
+              :style="{ width: '320px' }"
+              placeholder="选择编程语言"
+              allow-search
+            >
+              <a-option>java</a-option>
+              <a-option>python</a-option>
+              <a-option>golang</a-option>
+              <a-option>c++</a-option>
+              <a-option>javascript</a-option>
+              <a-option>php</a-option>
+            </a-select>
+          </a-form-item>
+        </a-form>
+        <CodeEditor
+          :value="form.code as string"
+          :language="form.language"
+          :handle-change="changeCode"
+        />
+        <a-button
+          type="primary"
+          style="min-width: 200px; margin-top: 20px"
+          @click="doSubmit"
+          >提交代码
+        </a-button>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, defineProps, withDefaults } from "vue";
+import {
+  QuestionControllerService,
+  type QuestionSubmitAddRequest,
+  QuestionSubmitControllerService,
+  QuestionVO,
+} from "../../../generated";
+import message from "@arco-design/web-vue/es/message";
+import CodeEditor from "@/components/CodeEditor.vue";
+import MdViewer from "@/components/MdViewer.vue";
+
+interface Props {
+  id: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  id: () => "",
+});
+
+const question = ref<QuestionVO>();
+
+const loadData = async () => {
+  const res = await QuestionControllerService.getQuestionVoByIdUsingGet(
+    props.id as any
+  );
+  if (res.code === 0) {
+    question.value = res.data;
+  } else {
+    message.error("加载失败." + res.message);
+  }
+};
+
+const form = ref<QuestionSubmitAddRequest>({
+  language: "java",
+  code: "",
+});
+
+const doSubmit = async () => {
+  if (!question.value?.id) {
+    return;
+  }
+  const res = await QuestionSubmitControllerService.doQuestionSubmitUsingPost({
+    ...form.value,
+    questionId: question.value?.id,
+  });
+  if (res.code === 0) {
+    message.success("提交成功");
+  } else {
+    message.error("提交失败，" + res.message);
+  }
+};
+
+/**
+ *  页面加载时，请求数据
+ */
+onMounted(() => {
+  loadData();
+});
+
+const changeCode = (value: string) => {
+  form.value.code = value;
+};
+</script>
+<style>
+#viewQuestionView {
+  max-width: 1440px;
+  margin: 0 auto;
+}
+</style>
